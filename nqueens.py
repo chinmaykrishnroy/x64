22320bcc79acc9d4ab6adc2e7bfaec243d4899a3
Chinmay Krishn Roy <morpheusprefecttt@gmail.com>
2024-05-09 06:30:18 +0530

    Update nqueens.py

--- nqueens.py
+++ nqueens.py
@@ -1,105 +1,53 @@
-# Function to check if placing a queen at position (row, col) is safe
-def is_safe(row, col):
-    # Check if there is a queen in the same row or column
-    for i in range(N):
-        if chessboard[row][i] == 1 or chessboard[i][col] == 1:
-            return False
-    # Check diagonals
-    for i in range(N):
-        for j in range(N):
-            if (i + j == row + col) or (i - j == row - col):
-                if chessboard[i][j] == 1:
-                    return False
-    return True
+# Function to check if a queen can be placed at board[row][col]
+def is_position_safe(chessboard, row, col, size):
+    # Check this row on left side for any queens
+    for i in range(col):
+        if chessboard[row][i] == 1:
+            return False  # Return False if a queen is found
 
-# Function to solve N Queens problem using backtracking
-def solve_n_queens(num_queens):
-    # Base case: if all queens are placed, return True
-    if num_queens == 0:
-        return True
-    # Try placing a queen in each row
-    for row in range(N):
-        for col in range(N):
-            # Check if it's safe to place a queen at (row, col)
-            if is_safe(row, col) and chessboard[row][col] != 1:
-                # Place the queen at (row, col)
-                chessboard[row][col] = 1
-                # Recur to place rest of the queens
-                if solve_n_queens(num_queens - 1):
-                    return True
-                # If placing queen at (row, col) doesn't lead to a solution, backtrack
-                chessboard[row][col] = 0
-    return False
+    # Check upper diagonal on left side for any queens
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if chessboard[i][j] == 1:
+            return False  # Return False if a queen is found
 
-# Get input for number of queens
+    # Check lower diagonal on left side for any queens
+    for i, j in zip(range(row, size, 1), range(col, -1, -1)):
+        if chessboard[i][j] == 1:
+            return False  # Return False if a queen is found
+
+    return True  # Return True if no queens are found, and it's safe to place a queen
+
+# Function to solve the N Queen problem using backtracking
+def place_queens(chessboard, col, size, solutions):
+    # Base case: If all queens are placed, add the board to the solutions
+    if col >= size:
+        solutions.append([' '.join(str(i) for i in row) for row in chessboard])
+        return
+
+    # Try placing a queen in each row of the current column
+    for i in range(size):
+        if is_position_safe(chessboard, i, col, size):
+            chessboard[i][col] = 1  # Place a queen at the current position
+
+            # Recur to place the rest of the queens
+            place_queens(chessboard, col + 1, size, solutions)
+
+            # If placing queen doesn't lead to a solution, remove the queen (backtrack)
+            chessboard[i][col] = 0
+
+# Function to print all solutions
+def display_solutions(all_solutions):
+    for solution in all_solutions:
+        for row in solution:
+            print(row)  # Print each row of the board
+        print()  # Print a newline after each solution
+
+# Driver code to test the above functions
 print("Enter the number of queens")
-num_queens = int(input())
-
-# Initialize the chessboard
-chessboard = [[0] * num_queens for _ in range(num_queens)]
-
-# Set the number of queens globally
-N = num_queens
-
-# Call the function to solve N Queens problem
-if solve_n_queens(num_queens):
-    # Print the solution
-    for row in chessboard:
-        print(row)
-else:
-    print("No solution exists for {} queens".format(num_queens))
-
-
-
-'''
-# Function to check if placing a queen at position (row, col) is safe
-def is_safe(row, col):
-    # Check each position in the same row and column for a queen
-    for i in range(N):
-        if chessboard[row][i] == 1 or chessboard[i][col] == 1:
-            return False  # Return False if a queen is found in the same row or column
-    # Check diagonals for a queen
-    for i in range(N):
-        for j in range(N):
-            # Check if the position is on a diagonal of the current position (row, col)
-            if (i + j == row + col) or (i - j == row - col):
-                if chessboard[i][j] == 1:
-                    return False  # Return False if a queen is found on a diagonal
-    return True  # Return True if no queens are found in the same row, column, or diagonal
-
-# Function to solve the N Queens problem using backtracking
-def solve_n_queens(num_queens):
-    # If all queens are placed, the problem is solved
-    if num_queens == 0:
-        return True
-    # Try placing a queen in each cell of the board
-    for row in range(N):
-        for col in range(N):
-            # Check if placing a queen at (row, col) is safe
-            if is_safe(row, col) and board[row][col] != 1:
-                chessboard[row][col] = 1  # Place the queen at (row, col)
-                # Recursively attempt to place the rest of the queens
-                if solve_n_queens(num_queens - 1):
-                    return True  # If successful, return True
-                chessboard[row][col] = 0  # If not, remove the queen and backtrack
-    return False  # If no valid placement is found, return False
-
-# Prompt the user to enter the number of queens
-print("Enter the number of queens")
-num_queens = int(input())  # Read the number of queens from the user
-
-# Initialize the chessboard with all zeros (no queens placed)
-chessboard = [[0] * num_queens for _ in range(num_queens)]
-
-# Set the global variable N to the number of queens
-N = num_queens
-
-# Attempt to solve the N Queens problem
-if solve_n_queens(num_queens):
-    # If a solution is found, print the chessboard with queens placed
-    for row in chessboard:
-        print(row)
-else:
-    # If no solution exists, inform the user
-    print("No solution exists for {} queens".format(num_queens))
-'''
+board_size = int(input())
+print()
+if board_size in [2,3]: print("No solution exists for {}*{} board!".format(board_size,board_size))
+chessboard = [[0 for _ in range(board_size)] for _ in range(board_size)]  # Initialize the board with all zeros
+all_solutions = []  # List to store all the possible solutions
+place_queens(chessboard, 0, board_size, all_solutions)  # Generate all solutions
+display_solutions(all_solutions)  # Print all the solutions
